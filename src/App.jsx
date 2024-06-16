113b4e1ad3775229a45003cc1fb6ad859bd884f5
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-06-16 16:17:58 +0530

    created pages and Setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 // import React from 'react'
 
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
     <>
       <div className="app">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
       </div>
     </>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return <></>;
+};
+
+export default Home;
