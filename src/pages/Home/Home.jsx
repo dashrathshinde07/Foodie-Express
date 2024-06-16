baa7bc86493d684bf452d69d45e0a630c9a223ff
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-06-16 16:35:35 +0530

    Web header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Home.css";
+import Header from "../../components/Header/Header";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default Home;
