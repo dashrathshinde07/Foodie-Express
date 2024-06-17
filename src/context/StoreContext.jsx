85ed156b37fc0beebc1c05d4fc849e4f6a888b47
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-06-17 18:32:42 +0530

    #

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const contextValue = {};
+
+  return (
+    <StoreContextProvider value={contextValue}>
+      {props.children}
+    </StoreContextProvider>
+  );
+};
+
+export default StoreContextProvider;
