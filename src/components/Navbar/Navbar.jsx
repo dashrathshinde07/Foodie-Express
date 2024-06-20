b6b2cffafea4f2a5006605d82a7482f879fde4a9
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-06-20 10:32:03 +0530

    feat: Add NavLink, smooth scrolling behavior

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("Mobile-app");
@@ -10,15 +11,15 @@ const Navbar = () => {
       <div className="navbar">
         <img src={assets.logo} alt="" className="logo" />
         <ul className="navbar-menu">
-          <li
+          <Link to='/'
             onClick={() => setMenu("Home")}
             className={menu === "Home" ? "active" : ""}
           >
             Home
-          </li>
-          <li  onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</li>
-          <li  onClick={() => setMenu("Mobile-app")} className={menu === "Mobile-app" ? "active" : ""}>Mobile-app</li>
-          <li  onClick={() => setMenu("Contact Us")} className={menu === "Contact Us" ? "active" : ""}>Contact Us</li>
+          </Link>
+          <a href="#explore-menu"  onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</a>
+          <a href="#app-download"  onClick={() => setMenu("Mobile-app")} className={menu === "Mobile-app" ? "active" : ""}>Mobile-app</a>
+          <a href="#footer"  onClick={() => setMenu("Contact Us")} className={menu === "Contact Us" ? "active" : ""}>Contact Us</a>
         </ul>
         <div className="navbar-right">
           <img src={assets.search_icon} alt="" />
