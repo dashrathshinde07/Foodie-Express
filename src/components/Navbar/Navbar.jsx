0b0e61258963e71246490681a48856b1982d5ca1
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-06-21 17:37:59 +0530

    Cart page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,31 +3,58 @@ import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("Mobile-app");
 
   return (
     <>
       <div className="navbar">
-        <img src={assets.logo} alt="" className="logo" />
+        <Link to='/'>
+          {" "}
+          <img src={assets.logo} alt="" className="logo" />
+        </Link>
+
         <ul className="navbar-menu">
-          <Link to='/'
+          <Link
+            to="/"
             onClick={() => setMenu("Home")}
             className={menu === "Home" ? "active" : ""}
           >
             Home
           </Link>
-          <a href="#explore-menu"  onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</a>
-          <a href="#app-download"  onClick={() => setMenu("Mobile-app")} className={menu === "Mobile-app" ? "active" : ""}>Mobile-app</a>
-          <a href="#footer"  onClick={() => setMenu("Contact Us")} className={menu === "Contact Us" ? "active" : ""}>Contact Us</a>
+          <a
+            href="#explore-menu"
+            onClick={() => setMenu("Menu")}
+            className={menu === "Menu" ? "active" : ""}
+          >
+            Menu
+          </a>
+          <a
+            href="#app-download"
+            onClick={() => setMenu("Mobile-app")}
+            className={menu === "Mobile-app" ? "active" : ""}
+          >
+            Mobile-app
+          </a>
+          <a
+            href="#footer"
+            onClick={() => setMenu("Contact Us")}
+            className={menu === "Contact Us" ? "active" : ""}
+          >
+            Contact Us
+          </a>
         </ul>
         <div className="navbar-right">
           <img src={assets.search_icon} alt="" />
           <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
+            <Link to="/cart">
+              {" "}
+              <img src={assets.basket_icon} alt="" />
+            </Link>
+
             <div className="dot"></div>
           </div>
-          <button onClick={()=> setShowLogin(true)}>Sign In</button>
+          <button onClick={() => setShowLogin(true)}>Sign In</button>
         </div>
       </div>
     </>
