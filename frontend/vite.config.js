fcd50998c61f422336787542b8ceba895401e146
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-24 19:57:52 +0530

    #

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-})
+  resolve: {
+    alias: {
+      '@': '/src', // Allows imports using '@/Context/StoreContext.jsx'
+    },
+  },
+});
