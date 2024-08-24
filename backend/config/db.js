7e22be136b10bd6b54c2d7af0f9049813c3ce5a8
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-24 19:13:29 +0530

    #

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
-export const  connectDB = async () =>{
-
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
-   
-}
-
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
+export const connectDB = async () => {
+  await mongoose
+    .connect(`${process.env.MONGO_URI}/Foodie-Express-Database`)
+    .then(() => console.log("DB Connected"));
+};
