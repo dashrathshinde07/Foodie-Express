6aa83b6cb78c1ed1678788eb0ac0a5c2acd7d0b1
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-25 10:57:25 +0530

    done final

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>"Craving something delicious? Order your favorite meals from top-rated restaurants delivered hot and fresh to your doorstep. Enjoy fast delivery, easy tracking, and secure payments. Your hunger, our priority. Contact us anytime!"</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
