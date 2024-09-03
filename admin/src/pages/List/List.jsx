0b6047b85cd8e1ed0e97cfb67afcbd26be9569c4
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-09-03 12:31:27 +0530

    #

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -49,7 +49,7 @@ const List = () => {
         {list.map((item, index) => {
           return (
             <div key={index} className='list-table-format'>
-              <img src={`${url}/images/` + item.image} alt="" />
+              <img src={item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>{currency}{item.price}</p>
